a95b75fb8c31bbcad5e24f591d55c4f464edba68
Erik Campobadal <soc@erik.cat>
2020-06-03 08:24:40 +0200

    Updated

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,126 +1,84 @@
-import {
-    colorPalette,
-    mergeOptions,
-    Hooks as BaseHooks
-} from '@chartisan/chartisan'
-import { EChartTitleOption, EChartOption, EChartsOptionConfig } from 'echarts'
+import { colorPalette, Hooks as BaseHooks } from '@chartisan/chartisan'
+import { EChartTitleOption, EChartOption } from 'echarts'
 import { CC } from './index'
 
-function getLabel(
-    axis: EChartOption.XAxis | EChartOption.XAxis[] | undefined,
-    index: number
-): string {
-    if (!axis) return ''
-    if (Array.isArray(axis)) return (axis[0].data?.[index] as string) ?? ''
-    return (axis.data?.[index] as string) ?? ''
+function getLabel(axis: EChartOption.XAxis | EChartOption.XAxis[] | undefined, index: number): string {
+  if (!axis) return ''
+  if (Array.isArray(axis)) return (axis[0].data?.[index] as string) ?? ''
+  return (axis.data?.[index] as string) ?? ''
 }
 
 export class Hooks extends BaseHooks<CC> {
-    /**
-     * Sets the title of the chart.
-     *
-     * @param {(string | EChartTitleOption)} text
-     * @returns {this}
-     * @memberof Hooks
-     */
-    title(text: string | EChartTitleOption): this {
-        return this.options({
-            title: typeof text === 'string' ? { text } : text
-        })
-    }
+  /**
+   * Sets the title of the chart.
+   */
+  title(text: string | EChartTitleOption): this {
+    return this.options({
+      title: typeof text === 'string' ? { text } : text,
+    })
+  }
 
-    /**
-     * Set the axis options for a given chart. If a boolean is given, it determines
-     * if the axis should show or not.
-     *
-     * @param {(boolean
-     *             | { xAxis?: EChartOption.XAxis; yAxis: EChartOption.YAxis })} [value=true]
-     * @returns {this}
-     * @memberof Hooks
-     */
-    axis(
-        value:
-            | boolean
-            | { xAxis?: EChartOption.XAxis; yAxis: EChartOption.YAxis } = true
-    ): this {
-        return this.options({
-            ...(typeof value === 'boolean'
-                ? (value = { xAxis: { show: value }, yAxis: { show: value } })
-                : value)
-        })
-    }
+  /**
+   * Set the axis options for a given chart. If a boolean is given, it determines
+   * if the axis should show or not.
+   */
+  axis(value: boolean | { xAxis?: EChartOption.XAxis; yAxis: EChartOption.YAxis } = true): this {
+    return this.options({
+      ...(typeof value === 'boolean' ? (value = { xAxis: { show: value }, yAxis: { show: value } }) : value),
+    })
+  }
 
-    /**
-     * Determines the legend options of the chart.
-     *
-     * @param {(boolean | EChartOption.Legend)} [value=true]
-     * @returns {this}
-     * @memberof Hooks
-     */
-    legend(show: boolean | EChartOption.Legend = true): this {
-        return this.options({
-            legend: typeof show === 'boolean' ? { show } : show
-        })
-    }
+  /**
+   * Determines the legend options of the chart.
+   */
+  legend(show: boolean | EChartOption.Legend = true): this {
+    return this.options({
+      legend: typeof show === 'boolean' ? { show } : show,
+    })
+  }
 
-    /**
-     * Set the tooltip options for the chart.
-     *
-     * @param {(boolean | EChartOption.Tooltip)} [show=true]
-     * @returns {this}
-     * @memberof Hooks
-     */
-    tooltip(show: boolean | EChartOption.Tooltip = true): this {
-        return this.options({
-            tooltip: typeof show === 'boolean' ? { show } : show
-        })
-    }
+  /**
+   * Set the tooltip options for the chart.
+   */
+  tooltip(show: boolean | EChartOption.Tooltip = true): this {
+    return this.options({
+      tooltip: typeof show === 'boolean' ? { show } : show,
+    })
+  }
 
-    /**
-     * Determine the color of the datasets.
-     *
-     * @param {string[]} [colors=colorPalette]
-     * @returns {this}
-     * @memberof Hooks
-     */
-    colors(colors: string[] = colorPalette): this {
-        return this.custom(({ data }) => (data.color = colors) && data)
-    }
+  /**
+   * Determine the color of the datasets.
+   */
+  colors(colors: string[] = colorPalette): this {
+    return this.custom(({ data }) => (data.color = colors) && data)
+  }
 
-    /**
-     * Determines the datasets of the chart and their options.
-     *
-     * @param {(string | (string | EChartOption.Series)[])} types
-     * @returns {this}
-     * @memberof Hooks
-     */
-    datasets(types: string | (string | EChartOption.Series)[]): this {
-        return this.custom(({ data, merge }) => {
-            const t = Array.isArray(types)
-                ? types.map(e => (typeof e === 'string' ? { type: e } : e))
-                : [{ type: types }]
-            // Modify each dataset.
-            if (data.series) {
-                for (let i = 0; i < data.series.length; i++) {
-                    // Check if it's a special dataset.
-                    data.series[i] = merge(
-                        // @ts-ignore
-                        data.series[i],
-                        t[i % t.length]
-                    )
-                    if (t[i % t.length].type == 'pie') {
-                        // Pie datasets require the data to be formated in a different way.
-                        // @ts-ignore
-                        data.series[i].data = data.series[i]?.data?.map(
-                            (value: unknown, i: number) => ({
-                                value,
-                                name: getLabel(data.xAxis, i)
-                            })
-                        )
-                    }
-                }
-            }
-            return data
-        })
-    }
+  /**
+   * Determines the datasets of the chart and their options.
+   */
+  datasets(types: string | (string | EChartOption.Series)[]): this {
+    return this.custom(({ data, merge }) => {
+      const t = Array.isArray(types) ? types.map((e) => (typeof e === 'string' ? { type: e } : e)) : [{ type: types }]
+      // Modify each dataset.
+      if (data.series) {
+        for (let i = 0; i < data.series.length; i++) {
+          // Check if it's a special dataset.
+          data.series[i] = merge(
+            // @ts-ignore
+            data.series[i],
+            t[i % t.length]
+          )
+          if (t[i % t.length].type == 'pie') {
+            // Pie datasets require the data to be formated in a different way.
+            // @ts-ignore
+            data.series[i].data = data.series[i]?.data?.map((value: unknown, i: number) => ({
+              value,
+              name: getLabel(data.xAxis, i),
+            }))
+          }
+        }
+      }
+      return data
+    })
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,174 +1,124 @@
 import { Hooks } from './hooks'
 import { install, ResizeObserver } from 'resize-observer'
 import { init, ECharts, EChartOption } from 'echarts'
-import {
-    isHook,
-    ServerData,
-    isChartisan,
-    Chartisan as Base
-} from '@chartisan/chartisan'
+import { isHook, ServerData, isChartisan, Chartisan as Base } from '@chartisan/chartisan'
 
 export { Hooks as ChartisanHooks }
 
 /**
  * Used as an alias.
- *
- * @type {CC}
  */
 export type CC = EChartOption
 
 /**
- * Base chart class for ChartJS.
- *
- * @export
- * @class Chart
- * @extends {Base}
+ * Base chart class for Echarts.
  */
 export class Chartisan extends Base<CC> {
-    /**
-     * The chart canvas.
-     *
-     * @type {HTMLDivElement}
-     * @memberof Chartisan
-     */
-    div?: HTMLDivElement
+  /**
+   * The chart canvas.
+   */
+  div?: HTMLDivElement
 
-    /**
-     * Stores the chart instance.
-     *
-     * @type {ECharts}
-     * @memberof Chartisan
-     */
-    chart?: ECharts
+  /**
+   * Stores the chart instance.
+   */
+  chart?: ECharts
 
-    /**
-     * Stores the resize observer.
-     *
-     * @type {ResizeObserver}
-     * @memberof Chartisan
-     */
-    observer?: ResizeObserver
+  /**
+   * Stores the resize observer.
+   */
+  observer?: ResizeObserver
 
-    /**
-     * Observes the chart division for changes to resize
-     * the chart to the new division dimensions.
-     *
-     * @protected
-     * @memberof Chartisan
-     */
-    protected observe() {
-        // Remove existing observations.
-        if (this.observer) this.observer.disconnect()
-        // Create a new observer with the given callback.
-        this.observer = new ResizeObserver(() => this.chart?.resize())
-        // Observe the division in case it exists.
-        if (this.div) this.observer.observe(this.div)
+  /**
+   * Observes the chart division for changes to resize
+   * the chart to the new division dimensions.
+   */
+  protected observe() {
+    // Remove existing observations.
+    if (this.observer) this.observer.disconnect()
+    // Create a new observer with the given callback.
+    this.observer = new ResizeObserver(() => this.chart?.resize())
+    // Observe the division in case it exists.
+    if (this.div) this.observer.observe(this.div)
+  }
+
+  /**
+   * Renews the division where the chart lives.
+   */
+  protected renewDiv() {
+    if (this.div) this.body.removeChild(this.div)
+    this.div = document.createElement('div')
+    this.div.style.width = '100%'
+    this.div.style.height = '100%'
+    this.body.appendChild(this.div)
+    this.observe()
+  }
+
+  /**
+   * Formats the data of the request to match the data that
+   * the chart needs (acording to the desired front-end).
+   */
+  protected formatData(response: ServerData): CC {
+    return {
+      xAxis: { data: response.chart.labels },
+      yAxis: {},
+      series: response.datasets.map(({ name, values }) => ({
+        name,
+        type: 'bar',
+        data: values,
+      })),
     }
+  }
 
-    /**
-     * Renews the division where the chart lives.
-     *
-     * @protected
-     * @memberof Chartisan
-     */
-    protected renewDiv() {
-        if (this.div) this.body.removeChild(this.div)
-        this.div = document.createElement('div')
-        this.div.style.width = '100%'
-        this.div.style.height = '100%'
-        this.body.appendChild(this.div)
-        this.observe()
-    }
+  /**
+   * Handles a successfull response of the chart data.
+   */
+  protected onUpdate(data: CC) {
+    if (this.chart) this.chart.dispose()
+    this.renewDiv()
+    this.chart = init(this.div!)
+    this.chart.setOption(data)
+  }
 
-    /**
-     * Formats the data of the request to match the data that
-     * the chart needs (acording to the desired front-end).
-     *
-     * @protected
-     * @param {ServerData} response
-     * @returns {ChartConfiguration}
-     * @memberof Chartisan
-     */
-    protected formatData(response: ServerData): CC {
-        return {
-            xAxis: { data: response.chart.labels },
-            yAxis: {},
-            series: response.datasets.map(({ name, values }) => ({
-                name,
-                type: 'bar',
-                data: values
-            }))
-        }
-    }
+  /**
+   * Handles a successfull response of the chart data
+   * in the background (possibly, updating the values
+   * of the chart without creating a new one).
+   */
+  protected onBackgroundUpdate(data: CC) {
+    if (this.chart) this.chart.setOption(data)
+    else this.onUpdate(data)
+  }
 
-    /**
-     * Handles a successfull response of the chart data.
-     *
-     * @protected
-     * @param {CC} data
-     * @memberof Chartisan
-     */
-    protected onUpdate(data: CC) {
-        if (this.chart) this.chart.dispose()
-        this.renewDiv()
-        this.chart = init(this.div!)
-        this.chart.setOption(data)
-    }
-
-    /**
-     * Handles a successfull response of the chart data
-     * in the background (possibly, updating the values
-     * of the chart without creating a new one).
-     *
-     * @protected
-     * @param {CC} data
-     * @param {ChartUpdateProps} [options]
-     * @memberof Chartisan
-     */
-    protected onBackgroundUpdate(data: CC) {
-        if (this.chart) this.chart.setOption(data)
-        else this.onUpdate(data)
-    }
-
-    /**
-     * Destroys the chart instance if any.
-     *
-     * @memberof Chartisan
-     */
-    destroy() {
-        if (this.chart) this.chart.dispose()
-    }
+  /**
+   * Destroys the chart instance if any.
+   */
+  protected onDestroy() {
+    if (this.chart) this.chart.dispose()
+  }
 }
 
 declare global {
+  /**
+   * Extends the Window interface.
+   */
+  interface Window {
     /**
-     * Extends the Window interface.
-     *
-     * @interface Window
+     * The chartisan class to initiate it as a global variable
+     * bound to the window var.
      */
-    interface Window {
-        /**
-         * The chartisan class to initiate it as a global variable
-         * bound to the window var.
-         *
-         * @type {isChartisan<CC>}
-         * @memberof Window
-         */
-        Chartisan: isChartisan<CC>
+    Chartisan: isChartisan<CC>
 
-        /**
-         * Determines the hooks of the chart.
-         *
-         * @type {isHook<CC>}
-         * @memberof Window
-         */
-        ChartisanHooks: isHook<CC>
-    }
+    /**
+     * Determines the hooks of the chart.
+     */
+    ChartisanHooks: isHook<CC>
+  }
 }
 
 if (typeof window !== 'undefined') {
-    // Install the ResizeObserver pollyfill.
-    if (!window.hasOwnProperty('ResizeObserver')) install()
-    window.Chartisan = Chartisan
-    window.ChartisanHooks = Hooks
+  // Install the ResizeObserver pollyfill.
+  if (!window.hasOwnProperty('ResizeObserver')) install()
+  window.Chartisan = Chartisan
+  window.ChartisanHooks = Hooks
 }
